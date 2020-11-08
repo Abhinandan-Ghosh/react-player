8824f99a1b8f5cbe7b74dfd07771a850ec12c876
Abhinandan-Ghosh <35860824+Abhinandan-Ghosh@users.noreply.github.com>
2020-11-08 23:00:39 +0530

    Added play and pause and time functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React, { useState } from "react";
+
 //import Styles
 import "./styles/app.scss";
 //adding components
 import Song from "./components/Song";
 import Player from "./components/Player";
+//import util
+import data from "./util";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,22 +1,60 @@
 //This component contains the player.
 //Like the time, play progress slider, play pause buttons etc to control the songs
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaPlay, FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  const audioRef = useRef(null);
+  //Event Handlers
+  const playSongHandler = () => {
+    // console.log(audioRef.current);
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ currentTime: current, duration: duration });
+  };
+
+  //function to format the time displayed.
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
+  //State to get song time information
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
         <input type="range" name="" id="" />
-        <p>End time</p>
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <FaAngleLeft size="30" className="skip-back" />
-        <FaPlay size="30" className="play" />
+        <FaPlay onClick={playSongHandler} size="30" className="play" />
         <FaAngleRight size="30" className="skip-forward" />
       </div>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
